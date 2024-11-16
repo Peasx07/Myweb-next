d9ebfbe55d283b71f3a1a2020370d0aad4182859
pardparw <87960461+pardparw@users.noreply.github.com>
2024-11-16 20:58:40 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: 'app.sa.ac.th',
+          port: '',
+          pathname: '/gubguru/pages/registerseller/uploads/Peachoom/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
